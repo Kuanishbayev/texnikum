707cd76af67e0a44f43edc64aa5eb1d04ca85784
Ernazar <kuanishbayevyernazar@gmail.com>
2024-07-27 17:08:41 +0500

    MUI has been added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Stack } from '@mui/material';
 
 const Home = () => {
+    const [age, setAge] = useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
   return (
-    <div className='h-screen flex items-center'>
+    <div className='h-screen flex flex-col justify-center items-center'>
+        <div className='mb-4 text-xl'>
+            <span className='font-bold'>ARALBOYI MEDICINA HÁM TRANSPORT TEXNIKUMI</span>
+        </div>
         <form className="max-w-md mx-auto md:shadow p-10 md:border md:rounded-md">
+            <div className='flex justify-center mb-4'>
+                <span className='font-bold text-blue-300'>Hújjet tapsırıw ushın formanı toltırıń</span>
+            </div>
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -24,33 +42,51 @@ const Home = () => {
             </div>
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Telefon nomer</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Qosımsha telefon nomer</label>
                 </div>
             </div>
-            <div className="relative z-0 w-full mb-5">
-            <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900">Jónelisti tańlań</label>
-                <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                    <option disabled selected>Jónelisti tańlań</option>
-                    <option>Стоматология иши</option>
-                    <option>Ҳамширалик иши</option>
-                    <option>Марказлаштирилган пост оператори</option>
-                </select>
-            </div>
-            <div className="relative z-0 w-full mb-5">
-                <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900">Tálim túrin tańlań</label>
-                <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                    <option disabled selected>Tálim túrin tańlań</option>
-                    <option>Kúndizgi</option>
-                    <option>Sırtqı</option>
-                </select>
-            </div>
-            <div className='flex justify-end'>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Jiberiw</button>
+            <Stack spacing={2}>
+                <FormControl variant="standard" sx={{ minWidth: 120, width: '100%' }}>
+                    <InputLabel id="demo-simple-select-standard-label">Jónelisti tańlań</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={age}
+                        onChange={handleChange}
+                        label="Age"
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Стоматология иши</MenuItem>
+                    <MenuItem value={20}>Ҳамширалик иши</MenuItem>
+                    <MenuItem value={30}>Марказлаштирилган пост оператори</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl variant="standard" sx={{ minWidth: 120, width: '100%' }}>
+                    <InputLabel id="demo-simple-select-standard-label">Tálim túrin tańlań</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={age}
+                        onChange={handleChange}
+                        label="Age"
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={20}>Kúndizgi</MenuItem>
+                    <MenuItem value={30}>Sırtqı</MenuItem>
+                    </Select>
+                </FormControl>
+            </Stack>
+            <div className='flex justify-end mt-4'>
+                <Button type="submit" variant="contained" endIcon={<SendIcon />}>Jiberiw</Button>
             </div>
         </form>
     </div>
